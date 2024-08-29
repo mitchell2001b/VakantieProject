b45ed62a5fccc94a8bc80c883403abdaab6c697f
mitchell2001b <79633852+mitchell2001b@users.noreply.github.com>
2024-08-29 11:59:51 +0200

    new kubernetes files

--- VakantieProject/Program.cs
+++ VakantieProject/Program.cs
@@ -28,6 +28,13 @@ builder.Services.AddScoped<IHotelRepo, HotelRepo>();
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+    dbContext.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
